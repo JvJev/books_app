a5441b2f934a68dbaf1502e3d01680e1c21c3b57
Jevgenij Voronov <jevgenij.voronov@gmail.com>
2022-11-07 23:26:10 +0200

    started-to-add-form

---
+++ Frontend/src/components/Signin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Box, Typography } from "@mui/material";
+
+
+export default function Signin() {
+  return (
+    <div>Signin
+        <form>
+            <input></input>
+            <input></input>
+            <input></input>
+            <button></button>
+
+        </form>
+    </div>
+  )
+}
